4a095a866eac6d43603e079f555aba9b26d26f4e
Thiago Taboada <contact.thiago.taboada@gmail.com>
2024-10-19 02:22:39 -0300

    Slideeeerrrr :c

--- script.js
+++ script.js
@@ -1,38 +1,12 @@
-const btnLeft = document.querySelector(".btn-left"),
-      btnRight = document.querySelector(".btn-right"),
-      slider = document.querySelector("#slider"),
-      sliderSection = document.querySelectorAll(".slider-section");
-
-const totalWidth = sliderSection.length * 100;
-slider.style.width = `${totalWidth}%`;
-
-let operacion = 0,
-    counter = 0,
-    widthImg = 100 / sliderSection.length;
-
-btnLeft.addEventListener("click", moveToLeft);
-btnRight.addEventListener("click", moveToRight);
-
-setInterval(moveToRight, 7000);
-
-function moveToRight() {
-    counter++;
-    if (counter >= sliderSection.length) {
-        counter = 0;
-        operacion = 0;
-    } else {
-        operacion += widthImg;
-    }
-    slider.style.transform = `translateX(-${operacion}%)`;
-}
-
-function moveToLeft() {
-    counter--;
-    if (counter < 0) {
-        counter = sliderSection.length - 1;
-        operacion = widthImg * counter;
-    } else {
-        operacion -= widthImg;
-    }
-    slider.style.transform = `translateX(-${operacion}%)`;
-}
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+        },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
